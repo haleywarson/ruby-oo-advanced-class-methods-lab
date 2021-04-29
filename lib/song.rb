a8d49a4c08f2ae176dd75261b11157453f15e783
Haley Warson <haleywarson@gmail.com>
2021-04-29 15:33:59 -0600

    commit tree 46c1a617793da22fbad21a813e52943215d1499f

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,36 @@ class Song
     self.class.all << self
   end
 
+  def self.new_by_name name
+    @name = new.name
+  end
+
+  def self.create_by_name name
+    @name = name
+    @@all << self
+  end 
+
+  def self.find_by_name name
+    @@all.find(name == @name)
+  end
+
+  # def self.find_or_create_by_name name
+  #   @@all.includes?(name == @name)
+  #     ? name
+  #     : self.create.by.name name  
+  #   else 
+  # end
+
+  def self.alphabetical
+    @@all.sort_by { |ele, | }
+  end 
+
+  def self.new_from_filename filename 
+    @@all << "#{artist_name} - #{name}.mp3"
+  end 
+
+  def self.destroy_all  
+    self.all.clear
+  end 
+
 end
